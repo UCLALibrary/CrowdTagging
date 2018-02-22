d0e3499f6ae4014f05d91a941583b6c486b69214
JayKun <junkaiong11@gmail.com>
2018-02-22 14:55:57 -0800

    added all required models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore'
+
+export interface Todo { name: string; }
+export interface Book { title: string; author: string; genre: string; pages: number; publisher: string;}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  todoCollec: AngularFirestoreCollection<Todo>;
+  todos: Observable<Todo[]>;
+  todo: Observable<Todo>;
+
+  bookCollection: AngularFirestoreCollection<Book>;
+  books: Observable<Book[]>;
+
+  constructor(private afs: AngularFirestore){
+      this.todoCollec = afs.collection<Todo>('todos');
+      this.todos = this.todoCollec.valueChanges();
+      this.todo = this.afs.doc<Todo>('todos/1').valueChanges();
+      
+      this.bookCollection = afs.collection<Book>('books');
+      this.books = this.bookCollection.valueChanges();
+  }
+  
   title = 'app';
+  
+  ngOnInit(){
+  }
+
+  addTodo(name: string){
+      const todo: Todo = { name };
+      this.todoCollec.add(todo);
+  }
+  
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +15,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule // imports firebase/storage only needed for storage features
   ],
-  providers: [],
+  providers: [AngularFireDatabase],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
