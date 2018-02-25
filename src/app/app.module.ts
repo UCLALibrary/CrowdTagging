30f3f5bdf5ef5786b4d98914c5525febdbc779eb
HirdayGupta <hirday.gupta@gmail.com>
2018-02-25 14:32:42 -0800

    create about us component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TranscribeComponent } from './transcribe/transcribe.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    TranscribeComponent
+    TranscribeComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule
