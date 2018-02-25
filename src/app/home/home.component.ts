c445df2117270ae412d9471f94d2f56eb507a7c5
HirdayGupta <hirday.gupta@gmail.com>
2018-02-25 14:43:48 -0800

    Move appComponent contents to homeComponent and init title

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  title: string;
+  constructor() {
+    this.title = 'Crowd Tagging';
+  }
 
   ngOnInit() {
   }
