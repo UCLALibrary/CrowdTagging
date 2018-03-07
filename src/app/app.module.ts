7d08bae912045e02c8bb7e1c7c1c49e93143fe7b
JayKun <junkaiong11@gmail.com>
2018-03-07 00:12:01 -0800

    merged changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,7 +36,7 @@ const appRoutes: Routes = [
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
-    AngularFireStorageModule // imports firebase/storage only needed for storage features
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
     RouterModule.forRoot(appRoutes)
   ],
   providers: [AngularFireDatabase],
