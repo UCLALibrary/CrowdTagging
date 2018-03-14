5cb0dddbeef322e2a2235a78e6ad6a16a98470cd
J4yKay <jorgeisraelian@g.ucla.edu>
2018-03-14 14:23:52 -0700

    Include rotation, sets & update nav, gallery

--- src/app/transcribe/transcribe.component.ts
+++ src/app/transcribe/transcribe.component.ts
@@ -13,14 +13,18 @@ export class TranscribeComponent implements AfterViewInit {
   constructor() { }
 
   ngAfterViewInit() {
- 
+
     /* Select reusable HTML elements */
     var triangles    = Array.from(document.querySelectorAll('.triangle')),
         imgContainer = document.getElementById("viewContainer"),
-        imgConParent = imgContainer.parentElement,
         galleryImg   = document.getElementById("galleryImage"),
-        prevArrow    = imgConParent.querySelector("#prev") as any,
-        nextArrow    = imgConParent.querySelector("#next") as any;
+        prevArrow    = imgContainer.querySelector("#prev") as any,
+        nextArrow    = imgContainer.querySelector("#next") as any,
+        rotContainer = document.querySelector("#rotateContainer") as any,
+        rotateLeft   = imgContainer.querySelector("#rotateLeft") as any,
+        rotateRight  = imgContainer.querySelector("#rotateRight") as any,
+        statusBar    = imgContainer.querySelector("#progress") as any,
+        submitBtn    = document.getElementById("submit");
 
     /* Collapse on click for each data set */
     triangles.forEach(function(item){ 
@@ -35,53 +39,94 @@ export class TranscribeComponent implements AfterViewInit {
         });
     });
 
-    var images = [
-                    "../../assets/pup1.jpg", 
-                    "../../assets/pup2.jpg", 
-                    "../../assets/pup3.jpg", 
-                    "../../assets/pup4.jpg"
-                 ]
+    var index = 1, 
+        imageSetIndex = 1, // Depends on session value, so they can come back to same book, if they leave
+        currSetLength = 10; // Depends on length of data structure in database
 
-    var index = 0;
-
-    /* Update current image in gallery */
+    /* Update gallery */
     function renderImage(){
-        galleryImg.setAttribute("src", images[index]);
+        var currImagePath = determineImageName();
+        console.log(currImagePath);
         galleryImg.removeAttribute("style");
+        galleryImg.style.backgroundImage = "url('" + currImagePath + "')";
+        statusBar.innerHTML = index + "/" + currSetLength;
     }
 
-    /* Display previous/next arrows on hover */
+    /* Produce name of the image within all50 directory */
+    function determineImageName(){
+        var directory = "../../assets/all50/";
+        var imageSet = "EALJ00" + ((imageSetIndex < 10) ? (0 + "" + imageSetIndex) : imageSetIndex);
+        var imageInSet = "_0" + ((index < 10) ? (0 + "" + index) : index);
+        
+        return (directory + imageSet + imageInSet + ".jpg");
+    }
+
+    /* Reset translate transforms on buttons inside gallery */
+    function showThese(arr){
+        arr.forEach(function(item){
+            item.style.transform = "translate(0)";
+        });
+    }
+
+    /* Display arrow and rotation buttons on hover */
     imgContainer.addEventListener("mouseover", function(){
-        prevArrow.style.transform = "translate(0)";
-        nextArrow.style.transform = "translate(0)";
+        showThese([prevArrow, nextArrow, rotateLeft, rotateRight, statusBar]);
     });
 
     /* Hide previous/next arrows on hover */
     imgContainer.addEventListener("mouseout", function(){
         prevArrow.style.transform = "translate(-160%)";
         nextArrow.style.transform = "translate(160%)";
+        rotateLeft.style.transform = "translate(-160%)";
+        rotateRight.style.transform = "translate(160%)";
+        statusBar.style.transform = "translateY(100%)";
     });
 
     /* Handle lower-bound of gallery */
     prevArrow.addEventListener("click", function(){
-        if(index > 0)
-            index -= 1;
+        if(index > 1)
+            index--;
 
         renderImage();
     });
 
     /* Handle upper-bound of gallery */
     nextArrow.addEventListener("click", function(){
-        if(index < images.length - 1)
-            index += 1;
+        if(index < currSetLength)
+            index++;
 
         renderImage();
     });
 
+    var degree = 0;
+
+    /* Allow for left rotation */
+    rotateLeft.addEventListener("click", function(){
+        degree -= 90;
+        if(degree === -90) degree = 270;
+        rotContainer.style.transform = "rotate(" + degree + "deg)";
+    });
+
+    /* Allow for right rotation */
+    rotateRight.addEventListener("click", function(){
+        degree += 90;
+        if(degree === 360) degree = 0;
+        rotContainer.style.transform = "rotate(" + degree + "deg)";
+    });
+
+    /* Update current image and image set positions */
+    submitBtn.addEventListener("click", function(){
+        imageSetIndex++;
+        index = 1;
+        renderImage();
+    })
+
     /* Allow for zoom/pan functionality on gallery */
-    panzoom(galleryImg, {
-        maxZoom: 1.5,
+    var pan = panzoom(galleryImg, {
+        maxZoom: 4,
         minZoom: 0.5
     });
+
+    renderImage();
   }
 }
